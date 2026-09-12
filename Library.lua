280299b4ce634e63f61a514f09839644ad63d420
Severity <139249700+Severitysvc@users.noreply.github.com>
2026-09-12 14:22:02 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1358,6 +1358,7 @@ function Library:Window(Data)
 
 	Controller.OnDestroy = OnDestroy
 	Controller.OnMinimize = Minimize
+	Controller.Destroyed = false
 
 	Controller.MinimizeKeybind = Library.Get(Data, { "MinimizeKeybind" }, Enum.KeyCode.RightShift)
 	Controller.IsMinimized = Library.Get(Data, { "IsMinimized", "Minimized" }, false)
@@ -4155,11 +4156,6 @@ function Library:Window(Data)
 			end)
 
 			ElementBody.MouseButton1Click:Connect(function()
-				if Blank then
-					--// Open:Fire()
-					return
-				end
-								
 				Opened = not Opened
 
 				if Opened then
